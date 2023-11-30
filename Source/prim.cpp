bc335eb8fda520e6432e9636ddd54dd9f7b22572
우창 홍 <hwoochhh@naver.com>
2023-12-01 00:08:50 +0900

    prim

---
+++ Source/prim.cpp
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+#define INF 1000
+#define MAX_VERTICES 100
+
+
+
+//prim 알고리즘의 구조체
+typedef struct GraphType {
+	int n;
+	int weight[7][7];
+}GraphType;
+int distance[7];
+int selected[7];
+int get_min_vertex(int n) {
+	int v = 0, i;
+
+	for (i = 0; i < n; i++) {
+		if (!selected[i]) {
+			v = i;
+			break;
+		}
+	}
+
+	for (i = 0; i < n; i++) {
+		if (!selected[i] && (distance[i] < distance[v])) {
+			v = i;
+		}
+	}
+	return v;
+}
+void prim(GraphType* g, int s) {
+	int i, u, v;
+
+	for (int u = 0; u < g->n; u++) {
+		distance[u] = INF;
+		selected[u] = 0;
+	}
+	distance[s] = 0;
+
+	for (i = 0; i < g->n; i++) {
+		u = get_min_vertex(g->n);
+		selected[u] = TRUE;
+
+		if (distance[u] == INF) {
+			return;
+		}
+
+		printf("정점 %c 추가\n", u + 65);
+
+		for (v = 0; v < g->n; v++) {
+			if (g->weight[u][v] != INF) {
+				if (!selected[v] && g->weight[u][v] < distance[v]) {
+					distance[v] = g->weight[u][v];
+				}
+			}
+		}
+	}
+}
+int main() {
+	GraphType g = { 7,
+		{   {0,7,INF,5,INF,INF,INF},
+			{7,0,8,9,7,INF,INF},
+			{INF,8,0,INF,5,INF,INF},
+			{5,9,INF,0,15,6,INF},
+			{INF,7,5,15,0,8,9},
+			{INF,INF,INF,6,8,0,11},
+			{INF,INF,INF,INF,9,11,0}
+		}
+	};
+	prim(&g, 0);
+}
